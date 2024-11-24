19c7197808ad539379844046981a5063bd9ce83f
Mykyta Lukianytsia <mykyta.lukianytsia@student.tuke.sk>
2024-11-24 01:39:23 +0100

    Creating dabases on starup, sql querying integrated

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,22 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from schemas import Message
+from utils import get_sql_answer, setup_databases
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    setup_databases()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/get-analytics")
 async def get_analytics(message: Message):
-    pass
+    #db_name = get_database_name()
+    db_name = "SPI_index"
+    sql_answer = await get_sql_answer(message.text, db_name)
+    return sql_answer
+
